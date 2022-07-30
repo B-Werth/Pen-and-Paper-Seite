bffd53888dd921f8ddc3148ee33d8acbba4278f0
B-Werth <bastian619werth@gmail.com>
2022-07-31 01:45:55 +0200

    m

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,29 @@
+import * as React from "react";
+import { red } from "@mui/material/colors";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Button } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import Navbar from "./Components/navbar";
+
+const darktheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: red[500],
+    },
+  },
+});
+
 function App() {
-  return <div className="App">yo</div>;
+  return (
+    <ThemeProvider theme={darktheme}>
+      <CssBaseline />
+      <main>
+        <Button variant="contained">yo</Button>
+      </main>
+    </ThemeProvider>
+  );
 }
 
 export default App;
